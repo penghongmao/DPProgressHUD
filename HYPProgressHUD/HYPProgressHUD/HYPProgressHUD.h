705b219f61afd96752a0f2bdd9cc01066bbe821b
dapeng <1832431688@qq.com>
2018-06-28 17:27:09 +0800

    summary0
    ceshi0

---
+++ HYPProgressHUD/HYPProgressHUD/HYPProgressHUD.h
@@ -0,0 +1,13 @@
+//
+//  HYPProgressHUD.h
+//  HYPProgressHUD
+//
+//  Created by 毛宏鹏 on 2018/6/22.
+//  Copyright © 2018年 sfbm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HaoEasyMBProgressHUD.h"
+@interface HYPProgressHUD : NSObject
+
+@end
---
+++ HYPProgressHUD/HYPProgressHUD/HYPProgressHUD.m
@@ -0,0 +1,13 @@
+//
+//  HYPProgressHUD.m
+//  HYPProgressHUD
+//
+//  Created by 毛宏鹏 on 2018/6/22.
+//  Copyright © 2018年 sfbm. All rights reserved.
+//
+
+#import "HYPProgressHUD.h"
+
+@implementation HYPProgressHUD
+
+@end
